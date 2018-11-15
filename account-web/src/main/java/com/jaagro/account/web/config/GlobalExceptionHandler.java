eef5680a19c6bc5bf72d2ec9c3559cd49e7d2dcb
yogasan <yoga330356233@126.com>
2018-11-15 14:09:31 +0800

    增加常量系统用户id

--- account-web/src/main/java/com/jaagro/account/web/config/GlobalExceptionHandler.java
+++ account-web/src/main/java/com/jaagro/account/web/config/GlobalExceptionHandler.java
@@ -20,21 +20,25 @@ import java.util.List;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(value=MethodArgumentNotValidException.class)
+    @ResponseBody
+    private BaseResponse methodArgumentNotValidExceptionHandler(HttpServletRequest request, Exception ex){
+        log.info("methodArgumentNotValidExceptionHandler",ex);
+        //处理Validation异常
+        StringBuffer errorMsg = new StringBuffer();
+        MethodArgumentNotValidException c = (MethodArgumentNotValidException) ex;
+        List<ObjectError> errors = c.getBindingResult().getAllErrors();
+        for (ObjectError error : errors) {
+            errorMsg.append(error.getDefaultMessage()).append(";");
+        }
+        return BaseResponse.errorInstance(ResponseStatusCode.QUERY_DATA_ERROR.getCode(), errorMsg.toString());
+    }
+
     @ExceptionHandler(value=Exception.class)
     @ResponseBody
     private BaseResponse exceptionHandler(HttpServletRequest request, Exception ex){
-        log.info("GlobalExceptionHandler",ex);
-        //处理Validation异常
-        if(ex instanceof MethodArgumentNotValidException){
-            StringBuffer errorMsg = new StringBuffer();
-            MethodArgumentNotValidException c = (MethodArgumentNotValidException) ex;
-            List<ObjectError> errors = c.getBindingResult().getAllErrors();
-            for (ObjectError error : errors) {
-                errorMsg.append(error.getDefaultMessage()).append(";");
-            }
-            return BaseResponse.errorInstance(ResponseStatusCode.QUERY_DATA_ERROR.getCode(), errorMsg.toString());
-        }
-        //处理服务器异常
+        log.info("exceptionHandler",ex);
         return BaseResponse.errorInstance(ResponseStatusCode.SERVER_ERROR.getCode(), ex.getMessage());
     }
+
 }
