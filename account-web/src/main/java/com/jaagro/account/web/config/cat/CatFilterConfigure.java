1020b28871e2a8d969002dfe554def5e5a6c4eeb
zhengxingtao <zheng_xigntao@126.com>
2018-11-20 16:32:04 +0800

    加入cat

---
+++ account-web/src/main/java/com/jaagro/account/web/config/cat/CatFilterConfigure.java
@@ -0,0 +1,42 @@
+package com.jaagro.account.web.config.cat;
+
+import com.dianping.cat.servlet.CatFilter;
+import org.apache.ibatis.plugin.Interceptor;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+/**
+ * @author tony
+ */
+@Configuration
+public class CatFilterConfigure {
+
+    @Bean
+    public FilterRegistrationBean catFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        CatFilter filter = new CatFilter();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        registration.setName("cat-filter");
+        registration.setOrder(1);
+        return registration;
+    }
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) throws IOException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        //dataSource
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        //mybatisPlugin
+        sqlSessionFactoryBean.setPlugins(new Interceptor[]{new CatMybatisPlugin()});
+        //mapperLocation
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:/mapper/*.xml"));
+        return sqlSessionFactoryBean;
+    }
+}
